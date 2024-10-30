384877b40d58487eaf5ff698160fd6c27115f8eb
James <james.auble@gmail.com>
2024-10-30 13:52:50 -0700

    added state to loan meta inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,10 @@ import { Input } from "@/components/ui/input"
 import DatePicker from '@/components/date-picker'
 
 export default function Home() {
-  const [loanAmount, setLoanAmount] = useState(0)
+  const [loanAmount, setLoanAmount] = useState(10000)
+  const [downPayment, setDownPayment] = useState(0)
+  const [loanTerm, setLoanTerm] = useState(36)
+  const [annualInterestRate, setAnnualInterestRate] = useState(6.0)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -35,16 +38,36 @@ export default function Home() {
           <TableBody>
             <TableRow className="font-medium">
               <TableCell>
-                <Input type="number" onChange={(e) => setLoanAmount(Number(e.target.value))} className="w-[150px]" />
+                <Input
+                  type="number"
+                  value={loanAmount}
+                  onChange={(e) => setLoanAmount(Number(e.target.value))}
+                  className="w-[150px]"
+                />
               </TableCell>
               <TableCell>
-                <Input className="w-[150px]" />
+                <Input
+                  type="number"
+                  value={downPayment}
+                  onChange={(e) => setDownPayment(Number(e.target.value))}
+                  className="w-[150px]"
+                />
               </TableCell>
               <TableCell>
-                <Input className="w-[150px]" />
+                <Input
+                  type="number"
+                  value={loanTerm}
+                  onChange={(e) => setLoanTerm(Number(e.target.value))}
+                  className="w-[150px]"
+                />
               </TableCell>
               <TableCell>
-                <Input className="w-[150px]" />
+                <Input
+                  type="number"
+                  value={annualInterestRate.toFixed(1)}
+                  onChange={(e) => setAnnualInterestRate(Number(e.target.value))}
+                  className="w-[150px]"
+                />
               </TableCell>
               <TableCell>
                 <DatePicker className="w-[200px]" />
